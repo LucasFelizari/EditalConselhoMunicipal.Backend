1d551152a3ee6eed87b5d22c50de715354a234df
Gustavo de Pinho <depinhogustavo7@outlook.com>
2022-11-21 20:58:24 -0300

    Primeiro Commit Teste

--- EditalConselho.Api/Startup.cs
+++ EditalConselho.Api/Startup.cs
@@ -34,7 +34,7 @@ namespace EditalConselho.Api
             });
             services.AddControllers();
 
-            //ALTERAR AQUI CONEXÃO DO BANCO // TESTE NOVAMENTE
+            //ALTERAR AQUI CONEXÃO DO BANCO
             services.AddDbContext<EditalContexto>(options => options.UseMySql(Configuration.GetConnectionString("ConselhoDatabase")));
             
         }
