890bdbd1eed52bace7e166d181e06ee3d91fa3f5
Lucas <lucasfelizari@gmail.com>
2022-11-22 20:14:39 -0300

    repositorio

--- EditalConselho.Infra/Repositorio/UsuarioRepositorio.cs
+++ EditalConselho.Infra/Repositorio/UsuarioRepositorio.cs
@@ -1,9 +1,22 @@
-﻿using EditalConselho.Dominio.Interface;
+﻿using EditalConselho.Dominio;
+using EditalConselho.Dominio.Interface;
+using EditalConselho.Infra.Database;
+using System.Linq;
 
 namespace EditalConselho.Infra.Repositorio
 {
     public class UsuarioRepositorio : IUsuarioRepositorio
     {
+        private readonly EditalContexto _contexto;
 
+        public UsuarioRepositorio(EditalContexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public Usuario ObterUsuarioPorId(int idUsuario)
+        {
+            return _contexto.Usuario.Where(x => x.Id == idUsuario).FirstOrDefault();
+        }
     }
 }
